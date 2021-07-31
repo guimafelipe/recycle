00e3d8f804b4b8c1ff052d6cab399301f56f1b3d
Felipe Guimaraes <guima.felipec@gmail.com>
2021-07-31 13:00:21 -0300

    adding pato

--- Scenes/Inimigos/Marreco/Marreco.gd
+++ Scenes/Inimigos/Marreco/Marreco.gd
@@ -2,9 +2,10 @@ extends KinematicBody2D
 
 enum States{
 	IDLE,
-	SEGUINDO,
+	RANGED,
+	MELEE,
+	MORTO
 	ATACANDO,
-	MORTO,
 	BOUNCING,
 	RECUPERANDO
 }
@@ -13,17 +14,20 @@ export(int) var hp = 10
 export(int) var max_hp = 20
 export(int) var atk_dmg = 13
 
-
 export(int) var speed = 100
 export(int) var ataque_speed = 300
 export(int) var bouncing_speed = 70
 
 export(int) var range_ataque = 70
-export(int) var range_visao = 150
 
 export(float) var cooldown_ataque = 2.0
 export(float) var bouncing_duration = 0.25
 
+export(int) var range_tiro = 150
+export(float) var cooldown_tiro = 1.0
+export(int) var dano_tiro = 5
+
+export(PackedScene) var bullet_scene
 export(Resource) var player_state = player_state as PlayerState
 
 var alvo : Vector2
@@ -32,22 +36,26 @@ var state = States.IDLE
 var player : Amdre
 var bouncing_from : Vector2
 
+
 func _ready():
-	$Visao/CollisionShape2D.shape.radius = range_visao
-	$Ataque/CollisionShape2D.shape.radius = range_ataque
+	$Ranged/CollisionShape2D.shape.radius = range_tiro
+	$Melee/CollisionShape2D.shape.radius = range_ataque
 	$BouncingTimer.wait_time = bouncing_duration
+	$TiroCooldown.wait_time = cooldown_tiro
 
 
 func _physics_process(delta : float):
 	if player_state.game_state != Enums.GameState.FREE:
 		$BouncingTimer.set_paused(true)
 		$RecuperandoTimer.set_paused(true)
+		$TiroCooldown.set_paused(true)
 		return
 	
 	if state == States.IDLE:
 		idle()
-	elif state == States.SEGUINDO:
-		seguindo(delta)
+	elif state == States.RANGED:
+		$TiroCooldown.set_paused(false)
+		ranged()
 	elif state == States.ATACANDO:
 		atacando(delta)
 	elif state == States.BOUNCING:
@@ -63,24 +71,31 @@ func take_damage(dano : int):
 	hp -= dano
 	hp = max(0, hp)
 	if hp <= 0:
-		die()
+		call_deferred("die")
 
 
 func die():
 	state = States.MORTO
 
 
-func idle():
+func idle() -> void:
 	pass
 
 
-func seguindo(delta : float):
-	velocity = global_position.direction_to(player.global_position).normalized()*speed
-	#move_and_slide(velocity)
-	move_and_slide(velocity)
+func ranged() -> void:
+	if $TiroCooldown.time_left == 0:
+		atira()
+		$TiroCooldown.start()
 
 
-func atacando(delta : float):
+func atira() -> void:
+	var target = player.global_position
+	var bullet = bullet_scene.instance() as Bullet
+	bullet.set_alvo(target, global_position)
+	get_tree().root.add_child(bullet)
+
+
+func atacando(delta : float) -> void:
 	velocity = global_position.direction_to(alvo)*ataque_speed
 	var res := move_and_collide(velocity*delta)
 	if res:
@@ -105,55 +120,52 @@ func morto():
 	pass
 
 
-func _on_Visao_body_entered(body : Amdre):
-	if not body:
-		return
-	player = body
-	if state == States.IDLE:
-		state = States.SEGUINDO
-
-
-func _on_Visao_body_exited(body : Amdre):
-	if not body:
-		return
-	player = null
-	print("oi")
-	if state == States.SEGUINDO:
-		state = States.IDLE
-
-
-func _on_Ataque_body_entered(body : Amdre):
-	if not body:
+func rescan() -> void:
+	if state == States.MORTO:
 		return
 	
-	if state == States.SEGUINDO:
-		alvo = body.global_position
-		state = States.ATACANDO
-
-
-func _on_Ataque_body_exited(body : Amdre):
-	if not body:
-		return
-	if state == States.IDLE:
-		state = States.SEGUINDO
-
-
-func _on_BouncingTimer_timeout() -> void:
-	print("timer over")
-	rescan()
-
-var qnt = 0
-
-func rescan() -> void:
-	print("comecando rescan " + str(qnt))
-	qnt+=1
 	state = States.RECUPERANDO
 	$RecuperandoTimer.start(cooldown_ataque)
 	yield($RecuperandoTimer, "timeout")
 	if not player:
 		state = States.IDLE
-	elif $Ataque.overlaps_body(player):
+	elif $Melee.overlaps_body(player):
 		alvo = player.global_position
 		state = States.ATACANDO
-	elif $Visao.overlaps_body(player):
-		state = States.SEGUINDO
+	elif $Ranged.overlaps_body(player):
+		state = States.RANGED
+
+
+func _on_Ranged_body_entered(body : Amdre):
+	if not body:
+		return
+	player = body
+	if state == States.IDLE:
+		state = States.RANGED
+
+
+func _on_Ranged_body_exited(body : Amdre):
+	if not body:
+		return
+	player = null
+	if state == States.RANGED:
+		state = States.IDLE
+
+
+func _on_Melee_body_entered(body : Amdre):
+	if not body:
+		return
+	if state == States.RANGED:
+		alvo = body.global_position
+		state = States.ATACANDO
+
+
+func _on_Melee_body_exited(body : Amdre):
+	if not body:
+		return
+	if state == States.IDLE:
+		state = States.RANGED
+
+
+func _on_BouncingTimer_timeout():
+	rescan()
---
+++ Scenes/Inimigos/Tatu/Tatu.gd
@@ -0,0 +1,160 @@
+extends KinematicBody2D
+
+enum States{
+	IDLE,
+	SEGUINDO,
+	ATACANDO,
+	MORTO,
+	BOUNCING,
+	RECUPERANDO
+}
+
+export(int) var hp = 10
+export(int) var max_hp = 20
+export(int) var atk_dmg = 13
+
+
+export(int) var speed = 100
+export(int) var ataque_speed = 300
+export(int) var bouncing_speed = 70
+
+export(int) var range_ataque = 70
+export(int) var range_visao = 150
+
+export(float) var cooldown_ataque = 2.0
+export(float) var bouncing_duration = 0.25
+
+export(Resource) var player_state = player_state as PlayerState
+
+var alvo : Vector2
+var velocity := Vector2(0,0)
+var state = States.IDLE
+var player : Amdre
+var bouncing_from : Vector2
+
+func _ready():
+	$Visao/CollisionShape2D.shape.radius = range_visao
+	$Ataque/CollisionShape2D.shape.radius = range_ataque
+	$BouncingTimer.wait_time = bouncing_duration
+
+
+func _physics_process(delta : float):
+	if player_state.game_state != Enums.GameState.FREE:
+		$BouncingTimer.set_paused(true)
+		$RecuperandoTimer.set_paused(true)
+		return
+	
+	if state == States.IDLE:
+		idle()
+	elif state == States.SEGUINDO:
+		seguindo(delta)
+	elif state == States.ATACANDO:
+		atacando(delta)
+	elif state == States.BOUNCING:
+		$BouncingTimer.set_paused(false)
+		bouncing(delta)
+	elif state == States.RECUPERANDO:
+		$RecuperandoTimer.set_paused(false)
+	elif state == States.MORTO:
+		morto()
+
+
+func take_damage(dano : int):
+	hp -= dano
+	hp = max(0, hp)
+	print(hp)
+	if hp <= 0:
+		call_deferred("die")
+
+
+func die():
+	state = States.MORTO
+
+
+func idle():
+	pass
+
+
+func seguindo(delta : float):
+	velocity = global_position.direction_to(player.global_position).normalized()*speed
+	#move_and_slide(velocity)
+	move_and_slide(velocity)
+
+
+func atacando(delta : float):
+	velocity = global_position.direction_to(alvo)*ataque_speed
+	var res := move_and_collide(velocity*delta)
+	if res:
+		if res.collider is Amdre:
+			var body = res.collider as Amdre
+			body.take_damage(atk_dmg)
+			bouncing_from = res.position
+			state = States.BOUNCING
+			$BouncingTimer.start()
+			return
+	
+	if global_position.distance_to(alvo) < 10:
+		rescan()
+
+
+func bouncing(delta : float):
+	velocity = -global_position.direction_to(bouncing_from)*bouncing_speed
+	move_and_slide(velocity)
+
+
+func morto():
+	pass
+
+
+func _on_Visao_body_entered(body : Amdre):
+	if not body:
+		return
+	player = body
+	if state == States.IDLE:
+		state = States.SEGUINDO
+
+
+func _on_Visao_body_exited(body : Amdre):
+	if not body:
+		return
+	player = null
+	print("oi")
+	if state == States.SEGUINDO:
+		state = States.IDLE
+
+
+func _on_Ataque_body_entered(body : Amdre):
+	if not body:
+		return
+	
+	if state == States.SEGUINDO:
+		alvo = body.global_position
+		state = States.ATACANDO
+
+
+func _on_Ataque_body_exited(body : Amdre):
+	if not body:
+		return
+	if state == States.IDLE:
+		state = States.SEGUINDO
+
+
+func _on_BouncingTimer_timeout() -> void:
+	print("timer over")
+	rescan()
+
+
+func rescan() -> void:
+	if state == States.MORTO:
+		return
+	
+	state = States.RECUPERANDO
+	$RecuperandoTimer.start(cooldown_ataque)
+	yield($RecuperandoTimer, "timeout")
+	if not player:
+		state = States.IDLE
+	elif $Ataque.overlaps_body(player):
+		alvo = player.global_position
+		state = States.ATACANDO
+	elif $Visao.overlaps_body(player):
+		state = States.SEGUINDO
